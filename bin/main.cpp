33f2a4805850025bd2c0b82fba4860b9cd7fd5e4
antoniop-lm <antoniop.lm@hotmail.com>
2015-11-04 21:23:58 -0200

    Sequencial funcionando e imagens inseridas.

--- bin/main.cpp
+++ bin/main.cpp
@@ -4,8 +4,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#include "processamento.h"
-
 using namespace cv;
 using namespace std;
 
@@ -15,7 +13,7 @@ int main(int argc, char *argv[]) {
         return -1;
     }
     
-    Mat imagem;
+    Mat imagem, nova_imagem;
     imagem = imread(argv[2], CV_LOAD_IMAGE_COLOR);
     
     if(!imagem.data) {
@@ -23,21 +21,40 @@ int main(int argc, char *argv[]) {
         return -1;
     }
     
-    processamento *P = NULL;
-    
-    if (strcmp (argv[1], "thread") == 0)
-        P = new paralelo(&imagem);
-    else if (strcmp (argv[1], "normal") == 0)
-        P = new sequencial(&imagem);
+    imagem.copyTo(nova_imagem);
+
+    if (strcmp (argv[1], "thread") == 0) {
+        
+    }
+    else if (strcmp (argv[1], "normal") == 0) {
+        int i,j, linha, coluna;
+        for (i = 0; i < imagem.rows; i++) {
+            for (j = 0; j < imagem.cols; j++) {
+                float sum[3] = {0, 0, 0};
+                for(linha = (i - 2); linha <= (i + 2); linha++){
+                    for(coluna = (j - 2); coluna <= (j + 2); coluna++) {
+                        if ((linha >= 0 && linha < imagem.rows) && (coluna >= 0 && coluna < imagem.cols)) {
+                            sum[0] += imagem.at<Vec3b>(linha,coluna)[0];
+                            sum[1] += imagem.at<Vec3b>(linha,coluna)[1];
+                            sum[2] += imagem.at<Vec3b>(linha,coluna)[2];
+                        }
+                    }
+                }
+                nova_imagem.at<Vec3b>(i,j)[0] = sum[0]/25;
+                nova_imagem.at<Vec3b>(i,j)[1] = sum[1]/25;
+                nova_imagem.at<Vec3b>(i,j)[2] = sum[2]/25;
+            }
+        }
+    }
     
     char *nome = (char*) malloc ((strlen(argv[2])+1)*sizeof(char));
     strncpy(nome,argv[2],(strlen(argv[2])-4));
-    strcat(nome,".ppm");
+    strcat(nome,"new.jpg");
     
-    Mat nova_imagem = P->processando();
+    imshow("teste",nova_imagem);
+    waitKey(0);
     imwrite(nome,nova_imagem);
     
-    delete P;
     free(nome);
     
     return 0;
