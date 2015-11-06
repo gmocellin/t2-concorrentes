120a268313d2b12f7c8c4e840258409ee51565fc
gmocellin <giovane.mocellin@gmail.com>
2015-11-06 18:05:56 -0200

    Arrumei os testes e tentei começa a implementa o paralelo, falta o MPI e MP(ta zoado)

---
+++ bin/tests-thread.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# for each matrix file
+for test_file in $(ls images)
+do
+
+	total_time_single=0
+	total_time_multi=0
+
+	# ten times to have an average
+	for((i=0 ;i<10;i++))
+	do
+		(/usr/bin/time -f "\n%e" make run ARGS=\\"thread images/$test_file\\") 2>> results/tmp-$test_file 1>> results/result-$test_file
+		total_time_single=$(python -c "print ($total_time_single + $(tail -n 1 results/tmp-$test_file))")
+
+
+	done
+
+	echo images/$test_file
+	echo Average Singlethreaded Time: $(python -c "print (\"%.3f\" % ($total_time_single/10))")
+	echo 
+
+	#rm results/tmp-$test_file
+done
