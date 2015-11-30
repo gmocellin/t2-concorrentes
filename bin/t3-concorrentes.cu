8447f71602609c0a49207354ce0a8286239ec55a
gmocellin <giovane.mocellin@gmail.com>
2015-11-30 03:05:51 -0200

    Muitos error corrigidos e versão parcial do programa

--- bin/t3-concorrentes.cu
+++ bin/t3-concorrentes.cu
@@ -5,7 +5,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
-#define nThreads 1024
+#define nThreads 512
 
 using namespace std;
 using namespace cv;
@@ -17,20 +17,25 @@ __global__ void smoothCuda(unsigned char *imagemC, unsigned char *nova_imagemC, 
     int colunaPixel = posicaoPixel % (*cols);
 
     int linha, coluna;
-    if(posicaoPixel == 22){
+    /*if(posicaoPixel == 21010){
         printf("ANTES:\n");
         printf("posicaoPixel: %d\n", posicaoPixel);
         printf("linhaPixel: %d   colunaPixel: %d\n", linhaPixel, colunaPixel);
         printf("Pixel: B=%u  G=%u  R=%u\n", imagemC[(linhaPixel * (*cols) + colunaPixel)*3],
                                             imagemC[(linhaPixel * (*cols) + colunaPixel)*3+1],
                                             imagemC[(linhaPixel * (*cols) + colunaPixel)*3+2]);
-    }
+    }*/
     if(posicaoPixel < (*rows)*(*cols)) {
         int sum[3] = {0, 0, 0};
         for(linha = (linhaPixel - 2); linha <= (linhaPixel + 2); linha++){
             for(coluna = (colunaPixel - 2); coluna <= (colunaPixel + 2); coluna++) {
                 if ((linha >= 0 && linha < (*rows)) && (coluna >= 0 && coluna < (*cols))) {
-                    
+                    /*if(posicaoPixel == 21010 ){
+                        printf("linha %d   coluna %d -- B=%u G=%u R=%u\n", linha, coluna,
+                                            imagemC[(linhaPixel * (*cols) + colunaPixel)*3],
+                                            imagemC[(linhaPixel * (*cols) + colunaPixel)*3+1],
+                                            imagemC[(linhaPixel * (*cols) + colunaPixel)*3+2]);
+                    }*/ 
                     sum[0] += imagemC[(linha * (*cols) + coluna)*3];
                     sum[1] += imagemC[(linha * (*cols) + coluna)*3 + 1];
                     sum[2] += imagemC[(linha * (*cols) + coluna)*3 + 2];
@@ -41,14 +46,14 @@ __global__ void smoothCuda(unsigned char *imagemC, unsigned char *nova_imagemC, 
         nova_imagemC[(linhaPixel * (*cols) + colunaPixel)*3 + 1] = sum[1]/25;
         nova_imagemC[(linhaPixel * (*cols) + colunaPixel)*3 + 2] = sum[2]/25;
     }
-    if(posicaoPixel == 22){
+    /*if(posicaoPixel == 21010){
         printf("DEPOIS:\n");
         printf("posicaoPixel: %d\n", posicaoPixel);
         printf("linhaPixel: %d   colunaPixel: %d\n", linhaPixel, colunaPixel);
         printf("Pixel: B=%u  G=%u  R=%u\n", imagemC[(linhaPixel * (*cols) + colunaPixel)*3],
                                             imagemC[(linhaPixel * (*cols) + colunaPixel)*3+1],
                                             imagemC[(linhaPixel * (*cols) + colunaPixel)*3+2]);
-    }
+    }*/
 }
 
 int main(int argc, char *argv[]) {
@@ -97,13 +102,13 @@ int main(int argc, char *argv[]) {
    
     smoothCuda<<<nBlocks,nThreads>>>(imagemC, nova_imagemC, cols, rows);
     
-    //printf("TESTE 2 %u\n", nova_imagemC[10001]);
     cudaMemcpy(nova_imagem.data, nova_imagemC, imagem.total(), cudaMemcpyDeviceToHost);
     
+    printf("Nova imagem %u\n", nova_imagem.data[21010]);
     printf("TESTE 3\n");
     // salva a imagem final com outro nome
     char nome[100] = "new_";
-    strcat(nome, argv[2]);
+    strcat(nome, argv[1]);
     imwrite(nome,nova_imagem);
 
     cudaFree(imagemC);
